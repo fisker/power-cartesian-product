08ab42337e30a8f27aa617d3c6a50b5b5809162c
fisker Cheung <lionkay@gmail.com>
2021-11-05 21:21:00 +0800

    Update is-array-like.js

--- src/utils/is-array-like.js
+++ src/utils/is-array-like.js
@@ -1,6 +1,6 @@
-import isLength from './is-length'
-import isValue from './is-value'
-import isFunction from './is-function'
+import isLength from './is-length.js'
+import isValue from './is-value.js'
+import isFunction from './is-function.js'
 
 function isArrayLike(value) {
   return isValue(value) && !isFunction(value) && isLength(value.length)
